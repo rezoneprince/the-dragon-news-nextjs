8cbafccd90c646671e0aa22b6f9aea95bffeac54
rezone <rezoneprinceme@gmail.com>
2024-05-06 18:16:33 +0600

    category dynamic news show

--- src/app/categories/[categoriesId]/page.js
+++ src/app/categories/[categoriesId]/page.js
@@ -1,9 +1,63 @@
+import { getCategoryNews } from "@/utils/getCategoryNews";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
 import React from "react";
 
-const DynamicNewsPage = ({ params, searchParams }) => {
+const DynamicNewsPage = async ({ params, searchParams }) => {
+  const { data } = await getCategoryNews(searchParams.category);
+
   return (
-    <div>
-      <h1>Dynamic News {searchParams.category}</h1>
+    <div className="my-5">
+      <h1>
+        Dynamic News <b>{searchParams.category}</b> news: {data.length}
+      </h1>
+      <Grid
+        className="mt-5"
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      >
+        {data.map((news) => (
+          <Grid key={news.id} item xs={6}>
+            <Card>
+              <CardActionArea>
+                <CardMedia>
+                  <Image
+                    src={news.thumbnail_url}
+                    height={200}
+                    width={800}
+                    alt="topNews"
+                  />
+                </CardMedia>
+                <CardContent>
+                  <p className="w-[100px] bg-red-500 text-white px-2 rounded my-5">
+                    {news.category}
+                  </p>
+                  <Typography gutterBottom>{news.title}</Typography>
+                  <Typography gutterBottom className="my-4">
+                    <b>Author Name:</b> {news.author.name}
+                  </Typography>
+                  <Typography gutterBottom className="my-4">
+                    <b>Publish Date:</b>
+                    <span>{news.author.published_date}</span>
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {news.details}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
---
+++ src/utils/getCategoryNews.js
@@ -0,0 +1,9 @@
+export const getCategoryNews = async (category) => {
+  const res = await fetch(
+    `https://the-news-portal-server.vercel.app/news?category=${category}`,
+    {
+      catch: "no-store",
+    }
+  );
+  return res.json();
+};
